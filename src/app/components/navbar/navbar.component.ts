f7d69c88fe0ec5fc3002906eb8918ff7af420690
willys7 <wilfuentes0320@gmail.com>
2018-04-25 23:06:02 -0600

    add functions to logout and save some information in the local storage

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,13 +8,19 @@ import {AuthService} from '../../services/auth.service'
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private auth:AuthService) { }
+  constructor(public auth:AuthService) {
+    auth.handleAuthentication();
+  }
 
   ngOnInit() {
   }
 
   login(){
     this.auth.login();
+  }
+
+  logout(){
+    this.auth.logout();
   }
 
 }
